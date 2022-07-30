f92dc7b6f0e74e350f4ae17f587ef35edb393ec6
Sarah <vanwars@gmail.com>
2022-07-30 10:56:41 -0400

    updates

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ import './style.css'
 
 document.querySelector('form').addEventListener('submit', ev => {
     ev.preventDefault();
+
+    // get user inputs from the DOM:
     const name = document.querySelector('#name').value;
     const email = document.querySelector('#email').value;
     const comment = document.querySelector('#comment').value;
+    
+    // build the custom tag using template literals:
     const template = `
         <user-comment name="${name}" email="${email}" comment="${comment}"></user-comment>
     `;
+
+    // add the new tag to the DOM:
     document.querySelector('.comments').insertAdjacentHTML('afterbegin', template);
-})
+});
