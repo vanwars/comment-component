012daf18bcc646173326738c81fc62ac2ec409c6
Sarah <vanwars@gmail.com>
2022-07-30 10:53:48 -0400

    added custom compnent

--- comment.js
+++ comment.js
@@ -1,5 +1,21 @@
-class Header extends HTMLElement {
-    constructor() {
-      super();
+class Comment extends HTMLElement {
+  
+    connectedCallback() {
+      const rn = Math.random() * 100;
+      this.innerHTML = `
+        <section class="comment">
+          <div class="user-info">
+            <img src="https://picsum.photos/100?num=${rn}" alt="Your avatar!" class="avatar" />
+            <div>
+              <p>${this.getAttribute("name")}</p>
+              <p>${this.getAttribute("email")}</p>
+            </div>
+          </div>
+          <p class="comment-text">${this.getAttribute("comment")}</p>
+        </section>
+        `;
     }
 }
+  
+customElements.define("user-comment", Comment);
+  
--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 import './style.css'
 
 
+document.querySelector('form').addEventListener('submit', ev => {
+    ev.preventDefault();
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const comment = document.querySelector('#comment').value;
+    const template = `
+        <user-comment name="${name}" email="${email}" comment="${comment}"></user-comment>
+    `;
+    document.querySelector('.comments').insertAdjacentHTML('afterbegin', template);
+})
