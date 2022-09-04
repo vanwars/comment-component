2f5a058f67e1882eaec4166f42b877392d42d071
vanwars <vanwars@gmail.com>
2022-09-04 15:41:45 +0000

    Deploying to gh-pages from @ vanwars/comment-component@c54eec637d60f7b2a935e2a8a38b2a9749830533 🚀

--- sw.js
+++ sw.js
@@ -1,1 +1,153 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");if(workbox){console.log("Yay! Workbox is loaded \u{1F389}"),workbox.precaching.precacheAndRoute(["[{"revision":"caf6541c32aee7a9ae8d52b419836a85","url":"index.html"},{"revision":"54a88bea79585cb4216be742640703c3","url":"manifest.webmanifest"}]"]),workbox.routing.registerRoute(/(.*).(?:png|gif|jpg)/,workbox.strategies.networkFirst({cacheName:"images-cache",plugins:[new workbox.expiration.Plugin({maxEntries:50,maxAgeSeconds:30*24*60*60})]})),workbox.routing.registerRoute(/(.*).html/,workbox.strategies.networkFirst({cacheName:"html-cache",plugins:[new workbox.expiration.Plugin({maxEntries:50,maxAgeSeconds:30*24*60*60})]})),workbox.routing.registerRoute(/(.*).js/,workbox.strategies.cacheFirst({cacheName:"code-cache",plugins:[new workbox.expiration.Plugin({maxEntries:50,maxAgeSeconds:30*24*60*60})]})),workbox.routing.registerRoute(/(.*).css/,workbox.strategies.cacheFirst({cacheName:"css-cache",plugins:[new workbox.expiration.Plugin({maxEntries:50,maxAgeSeconds:30*24*60*60})]}));const r=workbox.strategies.networkFirst({cacheName:"html-cache",plugins:[new workbox.expiration.Plugin({maxEntries:50})]});workbox.routing.registerRoute(/\.html/,e=>r.handle(e)),workbox.routing.registerRoute(/(.*)\.(?:html)/,e=>r.handle(e).then(o=>{if(o){if(o.status===404)return caches.match("pages/404.html")}else return caches.match("pages/offline.html");return o}))}else console.log("Boo! Workbox didn't load \u{1F62C}");
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+
+// if (workbox) {
+//     console.log(`Yay! Workbox is loaded 🎉`);
+//     // make some changes to this function
+//     workbox.precaching.precacheAndRoute([
+//         'self.__WB_MANIFEST'
+//     ]); 
+    // workbox.routing.registerRoute(
+    //     /(.*).(?:png|gif|jpg)/,
+    //     workbox.strategies.networkFirst({
+    //       cacheName: 'images-cache',
+    //       plugins: [
+    //         new workbox.expiration.Plugin({
+    //           maxEntries: 50,
+    //           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    //         })
+    //       ]
+    //     })
+    // );
+
+    // workbox.routing.registerRoute(
+    //     /(.*).html/,
+    //     workbox.strategies.networkFirst({
+    //       cacheName: 'html-cache',
+    //       plugins: [
+    //         new workbox.expiration.Plugin({
+    //           maxEntries: 50,
+    //           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    //         })
+    //       ]
+    //     })
+    // );
+
+    // workbox.routing.registerRoute(
+    //     /(.*).js/,
+    //     workbox.strategies.cacheFirst({
+    //       cacheName: 'code-cache',
+    //       plugins: [
+    //         new workbox.expiration.Plugin({
+    //           maxEntries: 50,
+    //           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    //         })
+    //       ]
+    //     })
+    // );
+
+    // workbox.routing.registerRoute(
+    //     /(.*).css/,
+    //     workbox.strategies.cacheFirst({
+    //       cacheName: 'css-cache',
+    //       plugins: [
+    //         new workbox.expiration.Plugin({
+    //           maxEntries: 50,
+    //           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    //         })
+    //       ]
+    //     })
+    // );
+
+    // const htmlHandler = workbox.strategies.networkFirst({
+    //     cacheName: 'html-cache',
+    //     plugins: [
+    //       new workbox.expiration.Plugin({
+    //         maxEntries: 50,
+    //       })
+    //     ]
+    // });
+      
+    // workbox.routing.registerRoute(/\.html/, args => {
+    //     return htmlHandler.handle(args);
+    // });
+
+    // workbox.routing.registerRoute(/(.*)\.(?:html)/, args => {
+    //     return htmlHandler.handle(args).then(response => {
+    //       if (!response) {
+    //         return caches.match('pages/offline.html');
+    //       } else if (response.status === 404) {
+    //         return caches.match('pages/404.html');
+    //       }
+    //       return response;
+    //     });
+    // });
+    
+// } else {
+//     console.log(`Boo! Workbox didn't load 😬`);
+// }
+
+
+
+
+
+
+
+
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js')
+// // Note: Ignore the error that Glitch raises about workbox being undefined.
+// if (workbox) {
+//     console.log(self);
+//     console.log(`Yay! Workbox is loaded 🎉`);
+//     // make some changes to this function
+//     workbox.precaching.precacheAndRoute([
+//         'self.__WB_MANIFEST'
+//     ]); 
+//     // // To avoid async issues, we load strategies before we call it in the event listener
+//     // workbox.loadModule('workbox-core')
+//     // workbox.loadModule('workbox-routing')
+//     // workbox.loadModule('workbox-cacheable-response')
+//     // workbox.loadModule('workbox-strategies')
+//     // workbox.loadModule('workbox-expiration')
+//     // pageCache(); 
+//     // googleFontsCache(); 
+//     // staticResourceCache();
+//     // imageCache();
+//     // offlineFallback();
+
+//     // const cacheNames = workbox.core.cacheNames
+//     // const { registerRoute, setCatchHandler, setDefaultHandler } = workbox.routing
+//     // const { CacheableResponsePlugin } = workbox.cacheableResponse
+//     // const {
+//     // NetworkFirst,
+//     // StaleWhileRevalidate,
+//     // NetworkOnly,
+//     // } = workbox.strategies
+//     // const { ExpirationPlugin } = workbox.expiration
+
+//     // const cacheName = cacheNames.runtime
+// }
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+    // make some changes to this function
+    workbox.precaching.precacheAndRoute([
+        'self.__WB_MANIFEST'
+    ]); 
+}
+import {
+    pageCache,
+    imageCache,
+    staticResourceCache,
+    googleFontsCache,
+    offlineFallback,
+    } from 'workbox-recipes';
+    
+pageCache();
+
+googleFontsCache();
+
+staticResourceCache();
+
+imageCache();
+
+offlineFallback();
