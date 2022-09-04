d50ff2c8f7de534515ec9646761b5348a35ed567
Sarah <vanwars@gmail.com>
2022-09-04 09:52:39 -0400

    updated index.html

--- src/vite.config.js
+++ src/vite.config.js
@@ -17,7 +17,8 @@ export default defineConfig({
                 ],
             },
             devOptions: {
-              enabled: true
+              enabled: true,
+              base: ''
            }
         })
     ]
