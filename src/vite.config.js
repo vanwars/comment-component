80752eca96da246a8501f3fcdce071203e6c5626
Sarah <vanwars@gmail.com>
2022-09-05 10:15:17 -0400

    Hopefully this won't break my stuff

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,10 +1,18 @@
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
+import { resolve } from 'path'
+
 
 export default defineConfig({
-  // base: '',
+  base: '/',
   build: {
     outDir: "../dist",
+    rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          movies: resolve(__dirname, 'movies/index.html')
+        }
+      }
   },
 
   plugins: [
