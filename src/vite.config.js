5d9511b45ae6b0dab32ac6ac4f07f2b8bf663dca
Sarah <vanwars@gmail.com>
2022-09-05 10:19:40 -0400

    Prettier ran

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,18 +1,17 @@
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
-import { resolve } from 'path'
-
+import { resolve } from "path";
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   build: {
     outDir: "../dist",
     rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'index.html'),
-          movies: resolve(__dirname, 'movies/index.html')
-        }
-      }
+      input: {
+        main: resolve(__dirname, "index.html"),
+        movies: resolve(__dirname, "movies/index.html"),
+      },
+    },
   },
 
   plugins: [
