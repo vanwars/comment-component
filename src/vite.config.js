1a43b6c44b547ecabb1c89e765bf33c8a86e1434
Sarah <vanwars@gmail.com>
2022-09-11 10:38:38 -0400

    vite config update

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "",
   build: {
     outDir: "../dist",
     rollupOptions: {
